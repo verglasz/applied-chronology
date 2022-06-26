bd44399a7fb7d00afa4b342cd6978d13a812422d
Valerio Lomanto <valerio.lomanto@sns.it>
2022-06-26 22:28:54 +0200

    add data services

---
+++ client/src/app/services/application.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+import { Application } from '../models/application';
+import { baseUrl } from '../config';
+
+type StringDateApplication = Omit<Omit<Application, 'started'>, 'updated'> & {
+  started: string;
+  updated: string;
+};
+
+function normalizeDate(application: StringDateApplication): Application {
+  return {
+    ...application,
+    started: new Date(application.started),
+    updated: new Date(application.updated),
+  };
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApplicationService {
+  constructor(private http: HttpClient) {}
+
+  getAll(userId: number): Observable<Application[]> {
+    return this.http
+      .get<StringDateApplication[]>(`${baseUrl}/users/${userId}/applications/`)
+      .pipe(map((data) => data.map(normalizeDate)));
+  }
+  get(userId: number, id: number): Observable<Application> {
+    return this.http
+      .get<StringDateApplication>(
+        `${baseUrl}/users/${userId}/applications/${id}`
+      )
+      .pipe(map(normalizeDate));
+  }
+  create(userId: number, data: { company: string }): Observable<any> {
+    return this.http.post(`${baseUrl}/users/${userId}/applications/`, data);
+  }
+  update(userId: number, id: number, data: { notes: string }): Observable<any> {
+    return this.http.put(`${baseUrl}/users/${userId}/applications/${id}`, data);
+  }
+  delete(userId: number, id: number): Observable<any> {
+    return this.http.delete(`${baseUrl}/users/${userId}/applications/${id}`);
+  }
+}
---
+++ client/src/app/services/interview.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable } from 'rxjs';
+import { Interview } from '../models/interview';
+import { baseUrl } from '../config';
+
+type StringDateInterview = Omit<Interview, 'date'> & {
+  date: string;
+};
+
+function normalizeDate(interview: StringDateInterview): Interview {
+  return {
+    ...interview,
+    date: new Date(interview.date),
+  };
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class InterviewService {
+  constructor(private http: HttpClient) {}
+
+  getAll(userId: number, parentApplication?: number): Observable<Interview[]> {
+    const query = parentApplication ? `?application=${parentApplication}` : '';
+    return this.http
+      .get<StringDateInterview[]>(
+        `${baseUrl}/users/${userId}/interviews/${query}`
+      )
+      .pipe(map((data) => data.map(normalizeDate)));
+  }
+  get(userId: number, id: number): Observable<Interview> {
+    return this.http
+      .get<StringDateInterview>(`${baseUrl}/users/${userId}/interviews/${id}`)
+      .pipe(map(normalizeDate));
+  }
+  create(
+    userId: number,
+    data: { applicationId: number; date: Date; notes: string }
+  ): Observable<any> {
+    return this.http.post(`${baseUrl}/users/${userId}/interviews/`, data);
+  }
+  update(
+    userId: number,
+    id: number,
+    data: { date?: Date; notes?: string }
+  ): Observable<any> {
+    return this.http.put(`${baseUrl}/users/${userId}/interviews/${id}`, data);
+  }
+  delete(userId: number, id: number): Observable<any> {
+    return this.http.delete(`${baseUrl}/users/${userId}/interviews/${id}`);
+  }
+}
